11985aea06dc2e9198b536e575bcf38bcbf9b88f
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2020-10-04 21:22:30 +0300

    Update wwe_wins.R

--- wwe_wins.R
+++ wwe_wins.R
@@ -2,17 +2,14 @@ library(tidyverse)
 library(patchwork)
 library(ggridges)
 library(gt)
-rm(list=ls())
-source("C:\\Users\\Philippos\\Documents\\Projects\\Tools\\theme_white.R")
-source("C:\\Users\\Philippos\\Documents\\Projects\\Tools\\theme_black.R")
-theme_set(theme_white())
+theme_set(theme_minimal())
 
 
 
 wwe<-readxl::read_xlsx(file.choose()) ; wwe<-wwe[order(wwe$Item,decreasing = F),]
 
 # Brands-Gender
-source("C:\\Users\\Philippos\\Documents\\Projects\\WWE\\wwe_brands_gender.R")
+source("wwe_brands_gender.R")
 
 # Create dataframe for plot
 df<-wwe[(wwe$Brand!="205 Live"&wwe$Brand!="Other"),c(1,2,3,4,9,10)]
@@ -39,17 +36,18 @@ f <- ggplot(df[df$Gender=="Female",], aes(x = `Win%`, y = Brand,fill=Brand,
 
 p <- m+f+plot_annotation(title="WWE: Win % Per Brand, 2020",caption="cagematch.net")
 
+# ggsave the ridge plot
+
 # Table
 g<- df ; g$`Win%`<-g$`Win%`/100 ; g<-g[order(g$Matches,decreasing=T),]
 
 # Pics
-source("C:\\Users\\Philippos\\Documents\\Projects\\WWE\\wwe_pics.R")
+source("wwe_pics.R")
 
 # Tables
-source("C:\\Users\\Philippos\\Documents\\Projects\\WWE\\wwe_gt.R")
+source("wwe_gt.R")
 
-gtsave(gt_raw,"gt_raw.png");gtsave(gt_sd,"gt_sd.png")
-gtsave(gt_nxt,"gt_nxt.png");gtsave(gt_nxt_uk,"gt_nxt_uk.png")
+# gtsave the created tables
 
  
 
